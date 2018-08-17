b5ec2064b9858da44eba4e1a074919d627edad29
Jakub Schneller <jakub.schneller@gmail.com>
2018-08-17 15:48:26 +0200

    Base 1.6
    Added an option to add items into the shop without having to change the code.

--- app/presenters/ShopPresenter.php
+++ app/presenters/ShopPresenter.php
@@ -54,6 +54,16 @@ class ShopPresenter extends BasePresenter
         }
         $this->template->categories = $categoriesArray;
 
+        $items = $this->database->table("items")->where("category_id", $categoryid);
+        $itemsArray = [];
+        foreach ($items as $item) {
+            $itemsArray[$item->item_id] = [
+                'db' => $items,
+                'item' => $this->database->table('items')->get($item->item_id),
+                'itemsCount' => $this->database->table("items")->count()
+            ];
+        }
+        $this->template->items = $itemsArray;
     }
 
     public function renderEmptyCategory()
@@ -71,6 +81,24 @@ class ShopPresenter extends BasePresenter
 
     }
 
+    public function renderItem($itemid)
+    {
+
+        $categories = $this->database->table("categories");
+        $categoriesArray = [];
+        foreach ($categories as $category) {
+            $categoriesArray[$category->category_id] = [
+                'db' => $categories,
+                'category' => $this->database->table('categories')->get($category->category_id),
+                'categoriesCount' => $this->database->table("categories")->count()
+            ];
+        }
+        $this->template->categories = $categoriesArray;
+
+        $item = $this->database->table("items")->where("item_id", $itemid)->fetch();
+        $this->template->item = $item;
+    }
+
     public function handleDeleteCategory($categoryid)
     {
         $first_category = $this->database->table("categories")->min("category_id");
@@ -78,6 +106,43 @@ class ShopPresenter extends BasePresenter
         $this->database->table("categories")->get($categoryid)->delete();
         $this->flashMessage('Kategorie byla odstraněna.', 'block');
         $this->redirect('Shop:category', array('categoryid' => $first_category));
+    }
+
+    public function renderAddItem($categoryid)
+    {
+        $this->template->categoryid = $categoryid;
+    }
+
+    protected function createComponentAddItem()
+    {
+
+        $form = new form;
+        $form->addText("name")
+            ->setRequired("Vyplňte prosím název zboží");
+        $form->addText("description")
+            ->setRequired("Vyplňte prosím popis náhledu zboží");
+        $form->addText("price")
+            ->setRequired("Vyplňte prosím název zboží");
+        $form->addText("content")
+            ->setRequired("Vyplňte prosím detailní popis zboží");
+        $form->addSubmit("submit");
+        $form->onSuccess[] = [$this, 'addItemSuccess'];
+        return $form;
+    }
+
+    public function AddItemSuccess($form, $values)
+    {
+        $this->database->table("items")->insert([
+
+            'item_name'=> $values->name,
+            'item_description'=>$values->description,
+            'item_price'=>$values->price,
+            'item_content'=>$values->content,
+            'category_id'=>$this->getParameter('categoryid')
+        ]);
+
+        $this->flashMessage('Zboží bylo úspěšně přidáno.');
+        $this->redirect('Shop:category', array("categoryid" => $this->getParameter('categoryid')));
     }
 
 }
